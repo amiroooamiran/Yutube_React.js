5edf5fa8f03482c7440067afa60b97f512d98a95
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-05-30 16:10:09 +0330

    part two react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// this packeg
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// import statics
+import './static/index.css';
+
+// omponents
+function Amiran(){
+  return <h1>Welcome to my site</h1>
+}
+
+// Root
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <Amiran/>
+)
