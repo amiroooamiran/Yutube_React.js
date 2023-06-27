b84716e09d24bbdd66db70dfa32b1be6d6059d52
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-06-28 02:03:31 +0330

    crate Card Props in Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,20 +2,18 @@ import React from 'react'
 
 import '../static/cards.css'
 
-// importin Images
-import BMWImage from '../static/image/040_bmw_x5.jpg'
-
-function Cards() {
-  return (
-    <div className='Cards'>
-        <img src={BMWImage} alt='card-product-image' />
-        <h4>Title</h4>
-        <p>This DICE for Product</p>
-        <button>See more Product <i class="uil uil-eye"></i></button>
-        <br />
-        <button>add to card <i class="uil uil-shop"></i> </button>
-    </div>
-  )
+const CarCards = (props) =>{
+    const { image, title, desc} = props.cars;
+    return (
+        <div className='Cards'>
+            <img src={image} alt='card-product-image' />
+            <h4>{ title }</h4>
+            <p>{ desc }</p>
+            <button>See more Product <i class="uil uil-eye"></i></button>
+            <br />
+            <button>add to card <i class="uil uil-shop"></i> </button>
+        </div>
+      )
 }
 
-export default Cards
+export default CarCards
