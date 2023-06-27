816ec04d2fef51d453b967accb68d3fb74c6dbcd
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-06-27 23:46:18 +0330

    add main_slider section

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,20 +2,21 @@ import React from 'react'
 import SimpleImageSlider from 'react-simple-image-slider'
 
 const images = [
-    { url : "https://wallpapercave.com/wp/wp2599594.jpg" },
-    { url : "https://i.pinimg.com/originals/bc/f8/6d/bcf86da45229b593da4967a30315168c.jpg"}
+  { url: "https://wallpapercave.com/wp/wp2599594.jpg" },
+  { url: "https://i.pinimg.com/originals/bc/f8/6d/bcf86da45229b593da4967a30315168c.jpg" }
 ]
 function Slider() {
   return (
-    
-    <div >
+    <div className='Slider_main'>
+      <div className='Slider' >
         <SimpleImageSlider
-        width={890}
-        height={500}
-        images={images}
-        showBullets={true}
-        showNavs={true}
-         />
+          width={890}
+          height={500}
+          images={images}
+          showBullets={true}
+          showNavs={true}
+        />
+      </div>
     </div>
   )
 }
