62fc72bf47215368ab948dca35a9617be8e0b897
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-06-28 02:03:43 +0330

    Create Car lists

---
+++ src/data/CarData.js
@@ -0,0 +1,19 @@
+const CarData = [
+    {
+        image:'image/bmw3.jpeg',
+        title:'BMW x5 offrod one',
+        desc:'this desc for therd car',
+    },
+    {
+        image:'image/040_bmw_x5.jpg',
+        title:'BMW x5 offrod two',
+        desc:'this desc for therd car',
+    },
+    {
+        image:'image/BMWx4.jpeg',
+        title:'BMW x5 offrod three',
+        desc:'this desc for therd car',
+    },
+]
+
+export default CarData
